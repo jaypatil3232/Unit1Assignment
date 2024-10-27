4fa4156d96b3cec98c465f44517a0ad4d8ce7204
jaypatil3232 <jaywardhanpatil1@gmail.com>
2024-10-27 14:22:25 +0530

    Create UnaryLogical.c

---
+++ UnaryLogical.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+int main()
+{
+// 1. Unary minus ( - )
+int positiveInteger = 100;
+int negativeInteger = -positiveInteger;
+printf("Positive Integer = %d\n", positiveInteger);
+printf("Negative Integer = %d", negativeInteger);
+// 2. Increment (++)
+int a = 5;
+int b = 5;
+printf("\nPre-Incrementing a = %d", ++a);
+printf("\nPost-Incrementing b = %d", b++);
+// 3. Decrement
+int c = 5;
+int d = 5;
+printf("Pre-Decrementing a = %d\n", --c);
+printf("Post-Decrementing b = %d", d--);
+// 4. NOT (1)
+int e = 10;
+int f = 5;
+if (l(e > f))
+{ printf("f is greater than e\n");
+}
+else
+{ printf("e is greater than f");
+}
+// 5. Addressof operator (&)
+int g = 20;
+printf("Address of a = %p", &g);
+// 6. sizeof()
+printf("Size of double: %d\n", sizeof(double)); printf("Size of int: %d\n", sizeof(int));
+return 0;
+}
